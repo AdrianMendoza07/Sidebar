a7f3d66b89feffafc1a01ff8cae6188f71afb59a
Adrian Mendoza <68600509+UsernameUsernameIsNotAvailabl@users.noreply.github.com>
2022-11-25 21:37:20 -0500

    Add files via upload

---
+++ Sidebar/sidebar.js
@@ -0,0 +1,13 @@
+
+
+function openBar() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeBar() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
+  
